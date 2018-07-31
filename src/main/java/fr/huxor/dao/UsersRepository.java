bbf8c51fec2e54f1b3fe1b9daa80108aa57eb0c4
bikach <chakibhoud.io@gmail.com>
2018-07-31 16:29:37 +0200

    dosier dao

---
+++ src/main/java/fr/huxor/dao/UsersRepository.java
@@ -0,0 +1,10 @@
+package fr.huxor.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.huxor.entities.Users;
+
+
+public interface UsersRepository extends JpaRepository<Users, String>{
+
+}
