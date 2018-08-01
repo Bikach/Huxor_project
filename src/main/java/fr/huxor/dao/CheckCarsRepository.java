1087082b15d8700bfe74a71aa1481f3de09fccab
bikach <chakibhoud.io@gmail.com>
2018-08-01 11:20:47 +0200

    add CheckCarsRepository;java

---
+++ src/main/java/fr/huxor/dao/CheckCarsRepository.java
@@ -0,0 +1,9 @@
+package fr.huxor.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.huxor.entities.CheckCars;
+
+public interface CheckCarsRepository extends JpaRepository<CheckCars, String>{
+
+}
