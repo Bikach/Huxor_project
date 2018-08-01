7c9a203b2f4ee8dde40f1fcdee51d1de65f03c63
bikach <chakibhoud.io@gmail.com>
2018-08-01 11:21:48 +0200

    add LeaseAgreementsRepository;java

---
+++ src/main/java/fr/huxor/dao/LeaseAgreementsRepository.java
@@ -0,0 +1,9 @@
+package fr.huxor.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.huxor.entities.LeaseAgreements;
+
+public interface LeaseAgreementsRepository extends JpaRepository<LeaseAgreements, String>{
+
+}
